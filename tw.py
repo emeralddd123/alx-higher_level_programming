6254226cde52bfb7680a1118f1ecb7b10cb0f1d2
emeralddd123 <93847541+emeralddd123@users.noreply.github.com>
2022-12-21 02:56:37 +0100

    classes

--- tw.py
+++
@@ -1,9 +0,0 @@
-
-
-
-
-city = input("What is your city\n (chicago bla snecn )")
-
-while city.lower() not in ["chicago", "bla", "cnsnc"]:
-    print("make sure you ve chsen from options")
-    city = input("What is your city")
